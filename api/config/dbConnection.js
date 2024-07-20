74af7faa5e4b5346d5e2eb12df4c3bbe578a3f86
Luciano Silva <lcnsilvajf@gmail.com>
2024-07-20 14:04:01 -0300

    back-end removed

--- api/config/dbConnection.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { mongo } from "mongoose";
-
-async function dbConnection() {
-    mongoose.connect(process.env.MONGODB_URI);     
-    return mongoose.connection;
-}
-
-export default dbConnection;
